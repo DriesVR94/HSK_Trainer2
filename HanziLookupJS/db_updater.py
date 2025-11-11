3a9041e32e73a375672d74b83c646abe7b515b2f
Dries.Van Ranst <driesvanranst94@hotmail.com>
2025-11-11 11:36:07 +0100

    Added HSK 3.0 - Level 6.

--- HanziLookupJS/db_updater.py
+++ HanziLookupJS/db_updater.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 
 # Load your JSON file
-with open('Frontend\dist\HSK3_0__Level_5.json', 'r', encoding='utf-8') as f:
+with open('Frontend\dist\HSK3_0__Level_6.json', 'r', encoding='utf-8') as f:
     json_data = json.load(f)
 
 # Connect to SQLite database
@@ -15,7 +15,7 @@ cursor.execute("PRAGMA foreign_keys = ON;")
 # Insert each entry in order
 for entry in json_data:
     chinese = entry['chinese']
-    cursor.execute("INSERT INTO words (chinese, level_id) VALUES (?, ?)", (chinese, 11))
+    cursor.execute("INSERT INTO words (chinese, level_id) VALUES (?, ?)", (chinese, 12))
 
 # Commit and close
 conn.commit()

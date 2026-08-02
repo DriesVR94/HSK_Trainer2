0e72db5376073530b4e7a7a21cb3c60586f942b6
Dries.Van Ranst <driesvanranst94@hotmail.com>
2026-08-02 14:37:38 +0200

    Refactoring registration page.

---
+++ HanziLookupJS/Backend/static/JS/registration_page.js
@@ -0,0 +1,32 @@
+let client;
+
+window.onload = function () {
+    client = google.accounts.oauth2.initTokenClient({
+        client_id: "365777418021-krre441oq3u1rjv89vis4qkj592io06t.apps.googleusercontent.com",
+        scope: "email profile openid",
+        callback: async (response) => {
+            console.log("Access token:", response.access_token);
+
+            const res = await fetch("/auth/google", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(response)
+            });
+
+            const data = await res.json();
+
+            if (data.success) {
+                window.location.href = "/home_page";
+            } else {
+                alert("Google login failed");
+            }
+        }
+    });
+
+    document.getElementById("googleLoginBtn")
+        .addEventListener("click", function () {
+            client.requestAccessToken();
+        });
+};
